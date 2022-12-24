9bee3678e6c79ac744706dd00e9acfe3f4d2e4db
p0150n3d <aranjan7480@gmail.com>
2022-12-24 21:24:35 +0000

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,94 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+
+/**
+ * Search function
+ */
+
+const searchInput = document.querySelector("#searchbar > input")
+const searchButton = document.querySelector("#searchbar > button")
+
+const lookup = {"deepl":"https://deepl.com/","/4chan":"4chan.org/","/mail":"gmail.com/"}
+const engine = "startpage"
+const engineUrls = {
+  deepl: "https://www.deepl.com/translator#-/-/",
+  duckduckgo: "https://duckduckgo.com/?q=",
+  ecosia: "https://www.ecosia.org/search?q=",
+  google: "https://www.google.com/search?q=",
+  startpage: "https://www.startpage.com/search?q=",
+  youtube: "https://www.youtube.com/results?q=",
+}
+
+const isWebUrl = value => {
+  try {
+    const url = new URL(value)
+    return url.protocol === "http:" || url.protocol === "https:"
+  } catch {
+    return false
+  }
+}
+
+const getTargetUrl = value => {
+  if (isWebUrl(value)) return value
+  if (lookup[value]) return lookup[value]
+  return engineUrls[engine] + value
+}
+
+const search = () => {
+  const value = searchInput.value
+  const targetUrl = getTargetUrl(value)
+  window.open(targetUrl, "_self")
+}
+
+searchInput.onkeyup = event => event.key === "Enter" && search()
+searchButton.onclick = search
+
+/**
+ * inject bookmarks into html
+ */
+
+const bookmarks = [{"id":"Vpt6SxCvd3eD64qr","label":"design tools","bookmarks":[{"id":"HBSQ2E3Io7lLIecL","label":"pixlrx","url":"https://pixlr.com/x/"},{"id":"C6aVOJAImk0oOfoa","label":"image enlarger","url":"https://bigjpg.com/en"},{"id":"QkYVSpspSQUodp1f","label":"haikei","url":"https://app.haikei.app/"},{"id":"lkMkcgycxhtALMFS","label":"css gradients","url":"https://larsenwork.com/easing-gradients/"}]},{"id":"36RKYbw0ZAzldebT","label":"worth reading","bookmarks":[{"id":"yBJOGeTEvf0eniow","label":"happy hues","url":"https://www.happyhues.co/"},{"id":"EDBkxJusqDaOYs5k","label":"styled-components","url":"https://www.joshwcomeau.com/react/demystifying-styled-components/"},{"id":"vG6eNQTDiL3RBUCJ","label":"react docs","url":"https://reactjs.org/docs/getting-started.html"}]},{"id":"Cf6l2igAFdpwJglF","label":"sources","bookmarks":[{"id":"6elt7wZu8vQA1Syp","label":"icons","url":"https://feathericons.com/"},{"id":"qgCPBwD6aXuZcTox","label":"gif","url":"https://designyoutrust.com/2019/05/the-chill-and-retro-motion-pixel-art-of-motocross-saito/"}]}]
+
+const createGroupContainer = () => {
+  const container = document.createElement("div")
+  container.className = "bookmark-group"
+  return container
+}
+
+const createGroupTitle = title => {
+  const h2 = document.createElement("h2")
+  h2.innerHTML = title
+  return h2
+}
+
+const createBookmark = ({ label, url }) => {
+  const li = document.createElement("li")
+  const a = document.createElement("a")
+  a.href = url
+  a.innerHTML = label
+  li.append(a)
+  return li
+}
+
+const createBookmarkList = bookmarks => {
+  const ul = document.createElement("ul")
+  bookmarks.map(createBookmark).forEach(li => ul.append(li))
+  return ul
+}
+
+const createGroup = ({ label, bookmarks }) => {
+  const container = createGroupContainer()
+  const title = createGroupTitle(label)
+  const bookmarkList = createBookmarkList(bookmarks)
+  container.append(title)
+  container.append(bookmarkList)
+  return container
+}
+
+const injectBookmarks = () => {
+  const bookmarksContainer = document.getElementById("bookmarks")
+  bookmarksContainer.append()
+  bookmarks.map(createGroup).forEach(group => bookmarksContainer.append(group))
+}
+
+injectBookmarks()
